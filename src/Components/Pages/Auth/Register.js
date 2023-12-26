c39ddc35d4bc11d8c7d0d1ca61b55f53cf3c9fc0
mmal3k <mmalekrais@gmail.com>
2023-12-26 13:59:51 +0100

    add auth

--- src/Components/Pages/Auth/Register.js
+++ src/Components/Pages/Auth/Register.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import RegisterForm from './RegisterForm'
+
+
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className="flex flex-col md:flex-row justify-between gap-10 md:gap-20 items-center py-8 px-5 md:px-[80px] md:py-[60px] lg:px-[160px] lg:py-[80px] md:h-screen">
+      <img className="w-[50vw] md:w-[45vw] " src="illustrationRegister.png" alt="" />
+      <RegisterForm />
+    </div>
   )
 }
 
