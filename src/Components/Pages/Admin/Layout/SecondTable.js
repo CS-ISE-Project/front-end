8ec4a399dd8d0c7f6f1a9d38e641393f8dbb795f
mmal3k <mmalekrais@gmail.com>
2024-01-31 15:29:47 +0100

    add tables integration

--- src/Components/Pages/Admin/Layout/SecondTable.js
+++ src/Components/Pages/Admin/Layout/SecondTable.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function SecondTable  () {
     const headers = ["ID","Titre","Url","Auteurs","Institutions","Publication Date",""]
     const tableImg = <img src='BiSort.svg'/>
     const tableIcon = ["Titre" , "Url" , "Publication Date"]
     const [selectedRowIndex, setSelectedRowIndex] = useState(null);
+    const [tableData , setTableData] = useState([])
+    const [isLoading , setIsLoading] = useState(false)
+    const [error , setError] = useState()
+    const [page, setPage] = useState(0)
     const handleAction = (actionType, index) => {
         
         
@@ -16,45 +20,67 @@ function SecondTable  () {
         setSelectedRowIndex(selectedRowIndex === index ? null : index);
       }
 
-    const modData = [
-        {
-            ID:"123456",
-            Titre:"Nanoparticules ...",
-            Url:"https://google.dz",
-            Auteurs:"Maria Airth",
-            Institutions:"Google, Microsoft, Esi",
-            PublicationDate :"17-12-2023"
-        },
-        {
-            ID:"123456",
-            Titre:"Nanoparticules ...",
-            Url:"https://google.dz",
-            Auteurs:"Maria Airth",
-            Institutions:"Google, Microsoft, Esi",
-            PublicationDate :"17-12-2023"
-        },
-        {
-            ID:"123456",
-            Titre:"Nanoparticules ...",
-            Url:"https://google.dz",
-            Auteurs:"Maria Airth",
-            Institutions:"Google, Microsoft, Esi",
-            PublicationDate :"17-12-2023"
-        },
-        {
-            ID:"123456",
-            Titre:"Nanoparticules ...",
-            Url:"https://google.dz",
-            Auteurs:"Maria Airth",
-            Institutions:"Google, Microsoft, Esi",
-            PublicationDate :"17-12-2023"
-        },
-    ]
+      useEffect(() => {       
+
+        const fetchTableData = async ()=>{
+            setIsLoading(true)
+        try{
+            const response = await fetch(`https://ise-project-api-production.up.railway.app/users?page=${page}`)
+            const data = await response.json()
+            setTableData(data)
+        }catch (e){
+            setError(e)
+        }finally{
+            setIsLoading(false)
+        }   
+        }
+        fetchTableData()
+    }, [page])
+
+    
+    // const modData = [
+    //     {
+    //         ID:"123456",
+    //         Titre:"Nanoparticules ...",
+    //         Url:"https://google.dz",
+    //         Auteurs:"Maria Airth",
+    //         Institutions:"Google, Microsoft, Esi",
+    //         PublicationDate :"17-12-2023"
+    //     },
+    //     {
+    //         ID:"123456",
+    //         Titre:"Nanoparticules ...",
+    //         Url:"https://google.dz",
+    //         Auteurs:"Maria Airth",
+    //         Institutions:"Google, Microsoft, Esi",
+    //         PublicationDate :"17-12-2023"
+    //     },
+    //     {
+    //         ID:"123456",
+    //         Titre:"Nanoparticules ...",
+    //         Url:"https://google.dz",
+    //         Auteurs:"Maria Airth",
+    //         Institutions:"Google, Microsoft, Esi",
+    //         PublicationDate :"17-12-2023"
+    //     },
+    //     {
+    //         ID:"123456",
+    //         Titre:"Nanoparticules ...",
+    //         Url:"https://google.dz",
+    //         Auteurs:"Maria Airth",
+    //         Institutions:"Google, Microsoft, Esi",
+    //         PublicationDate :"17-12-2023"
+    //     },
+    // ]
+
+
   
     return (
 
 
     <div>
+        {isLoading && <div className='mx-auto mt-[200px] w-full text-center' >Loading....</div>}
+        {!isLoading && 
         <table className='mt-[88px] md:mt-[112px] w-[80vw] mx-auto text-left'>
             <thead className='bg-Blue66 text-white'>
                 <tr className=''>
@@ -69,8 +95,8 @@ function SecondTable  () {
                 </tr>
             </thead>
             <tbody className='text-Typo'>
-            {modData.map((data,i)=>(
-                    <tr key={i} className='border-b-2 border-solid border-Typo border-opacity-20'>
+            {tableData.map((data,i)=>(
+                <tr key={i} className='border-b-2 border-solid border-Typo border-opacity-20'>
                         <td className='p-4'>{data.ID}</td>
                         <td className='p-4'>{data.Titre}</td>
                         <td className='p-4'>{data.Url}</td>
@@ -85,7 +111,7 @@ function SecondTable  () {
                                 <div className='h-[10px] w-[10px] rounded-full bg-Typo'></div>
                             </button>
                             {selectedRowIndex === i && (
-                                    <div className={` absolute top-[60%] left-[60%] bg-white flex justify-center items-center py-2 w-[100px] shadow-2xl rounded-md `}>
+                                <div className={` absolute top-[60%] left-[60%] bg-white flex justify-center items-center py-2 w-[100px] shadow-2xl rounded-md `}>
                                             <button onClick={() => handleAction("block", i)}>Delete</button>
                                     </div>
                             )} 
@@ -95,6 +121,7 @@ function SecondTable  () {
                 ))} 
             </tbody>
         </table>
+        }
     </div>
   )
 }
--- src/Components/Pages/Admin/Layout/Table.js
+++ src/Components/Pages/Admin/Layout/Table.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 
 function Table (){
 
@@ -6,78 +6,38 @@ function Table (){
     
     const [selectedRowIndex, setSelectedRowIndex] = useState(null);
 
-  const toggleDivVisibility = (index) => {
-    setSelectedRowIndex(selectedRowIndex === index ? null : index);
-  }
+    const toggleDivVisibility = (index) => {
+        setSelectedRowIndex(selectedRowIndex === index ? null : index);
+    }
+
+    const [modData , setModData] = useState([])
+    const [isLoading , setIsLoading] = useState(false)
+    const [error , setError] = useState()
+    const [page, setPage] = useState(0)
  
 
+    useEffect(() => {       
 
-    const modData = [
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"actif"
-        },
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"actif"
-        },
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"actif"
-        },
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"bloque"
-        },
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"actif"
-        },
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"actif"
-        },
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"bloque"
-        },
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"actif"
-        },
-        {
-            ID:"123456",
-            Nom:"Bengherbia",
-            Prenom:"Karim",
-            Email:"la_bengherbia@esi.dz",
-            Status:"actif"
-        },
-        
-    ]
+        const fetchTableData = async ()=>{
+            setIsLoading(true)
+        try{
+            const response = await fetch(`https://ise-project-api-production.up.railway.app/users?page=${page}`)
+            const data = await response.json()
+            setModData(data)
+        }catch (e){
+            setError(e)
+        }finally{
+            setIsLoading(false)
+        }   
+        }
+        fetchTableData()
+    }, [page])
+
+
+    if(error){
+        return <div> Something went wrong ! please try again</div>
+    }
+    
 
     const tableImg = <img src='BiSort.svg'/>
     const tableIcon = ["Nom" , "Prenom" , "Status"]
@@ -95,7 +55,8 @@ function Table (){
 
   return (
     <div>
-        <table className='mt-[88px] md:mt-[112px] w-[80vw] mx-auto text-left'>
+        {isLoading && <div className='mx-auto mt-[200px] w-full text-center' >Loading....</div>}
+        {!isLoading && <table className='mt-[88px] md:mt-[112px] w-[80vw] mx-auto text-left'>
             <thead className='bg-Blue66 text-white'>
                 <tr className=''>
                     {headers.map((header,i)=>(
@@ -111,10 +72,10 @@ function Table (){
             <tbody className='text-Typo'>
                 {modData.map((data,i)=>(
                     <tr key={i} className='border-b-2 border-solid border-Typo border-opacity-20'>
-                        <td className='p-4'>{data.ID}</td>
-                        <td className='p-4'>{data.Nom}</td>
-                        <td className='p-4'>{data.Prenom}</td>
-                        <td className='p-4'>{data.Email}</td>
+                        <td className='p-4'>{data.id}</td>
+                        <td className='p-4'>{data.first_name}</td>
+                        <td className='p-4'>{data.last_name}</td>
+                        <td className='p-4'>{data.email}</td>
                         <td className='text-center p-4'> 
                             {data.Status === "actif" ?
                             <div className='flex items-center gap-4'>
@@ -135,7 +96,7 @@ function Table (){
                                 <div className='h-[10px] w-[10px] rounded-full bg-Typo'></div>
                             </button>
                             {selectedRowIndex === i && (
-                                    <div className={` absolute top-[60%] left-[60%] bg-white flex justify-center items-center py-2 w-[100px] shadow-2xl rounded-md `}>
+                                    <div className={` absolute top-[65%] left-[60%] bg-white flex justify-center items-center py-2 w-[100px] drop-shadow-special rounded-md `}>
                                         {data.Status === "actif" ? (
                                             <button onClick={() => handleAction("block", i)}>Block</button>
                                             ) : (
@@ -148,7 +109,8 @@ function Table (){
                     </tr>
                 ))}   
             </tbody>
-        </table>
+        </table>}
+       
     </div>
   )
 }
